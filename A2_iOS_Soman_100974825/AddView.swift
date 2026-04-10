355b955c23dbe624e429ed348e256192d86cc202
soman96 <ahmad@soman.ca>
2026-04-09 21:34:27 -0400

    add new product form

--- A2_iOS_Soman_100974825/AddView.swift
+++ A2_iOS_Soman_100974825/AddView.swift
@@ -5,3 +5,27 @@
 //  Created by Soman Ahmad on 2026-04-09.
 //
 
+import SwiftUI
+import CoreData
+
+struct AddView: View {
+    @Environment(\.managedObjectContext) private var context
+    @State private var name = ""
+    @State private var description = ""
+    @State private var priceText = ""
+    @State private var provider = ""
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Product Info") {
+                    TextField("Name", text: $name)
+                    TextField("Description", text: $description)
+                    TextField("Price)", text: $priceText)
+                    TextField("Provider", text: $provider)
+                }
+            }
+            .navigationTitle("Add Product")
+        }
+    }
+}
