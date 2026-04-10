6e06848c35c4e5d90d99223b9fa4eb75b7192475
soman96 <ahmad@soman.ca>
2026-04-09 20:35:50 -0400

    create all the views

---
+++ A2_iOS_Soman_100974825/AddView.swift
@@ -0,0 +1,7 @@
+//
+//  AddView.swift
+//  A2_iOS_Soman_100974825
+//
+//  Created by Soman Ahmad on 2026-04-09.
+//
+
---
+++ A2_iOS_Soman_100974825/BrowseView.swift
@@ -0,0 +1,7 @@
+//
+//  BrowseView.swift
+//  A2_iOS_Soman_100974825
+//
+//  Created by Soman Ahmad on 2026-04-09.
+//
+
