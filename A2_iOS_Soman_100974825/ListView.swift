e4022033f1248d60ae187c3a8cf1c69573b155d1
soman96 <ahmad@soman.ca>
2026-04-09 21:11:26 -0400

    basic lsit view

--- A2_iOS_Soman_100974825/ListView.swift
+++ A2_iOS_Soman_100974825/ListView.swift
@@ -5,3 +5,24 @@
 //  Created by Soman Ahmad on 2026-04-09.
 //
 
+import SwiftUI
+import CoreData
+
+struct ListView: View {
+    @Environment(\.managedObjectContext) private var context
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Product.name, ascending: true)],
+        animation: .default
+    ) private var products: FetchedResults<Product>
+
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(products, id: \.id) { product in
+                    Text(product.name ?? "Unknown")
+                }
+            }
+            .navigationTitle("All Products")
+        }
+    }
+}
